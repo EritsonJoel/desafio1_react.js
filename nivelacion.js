9b151d4f29bcb1e00898b2d14223a95c938d0e41
Joel Zeta <zetajoel2911@gmail.com>
2021-11-03 03:28:39 -0500

    guardar tdo

---
+++ nivelacion.js
@@ -0,0 +1,15 @@
+//
+
+
+//https://developer.mozilla.org/  //documentacion
+
+
+let nombre = 'federico' //
+
+var nom = 'fede' //no es recomendable: porq que convierte a la variable , en una variable local
+
+
+function saludar(nombre){
+    console.log(nombre)
+}
+saludar('juan')
